4adb037434fb6abbd401a029198896bb4a7548b2
Mehul Kumar <84561537+Mehulkumar123@users.noreply.github.com>
2023-02-01 20:32:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+import random
+import sys
+import requests
+import json
+from datetime import datetime
+from termcolor import colored
+import facts as Fact
+import random
+import time
+import itertools
+from tqdm import tqdm
