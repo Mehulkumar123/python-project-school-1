8dc87116a1e2078a47870b4adba64181c2e90a92
Mehul Kumar <84561537+Mehulkumar123@users.noreply.github.com>
2023-02-02 18:09:17 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import requests
 import re
-from datetime import datetime
 import ast
 import time
 import random
+import datetime
+import wikipedia
 import loading as load
 import facts as Fact
-from fuzzywuzzy import process
+from datetime import datetime
+from fuzzywuzzy import process, fuzz
 from termcolor import colored
-
-load.loading_animation(1)
 
 def answer_question(query, data_set):
     # Check if the answer is already in the data set
@@ -22,7 +22,7 @@ def answer_question(query, data_set):
         try:
             # Evaluate the mathematical expression and return the result
             expression = match.group(1)
-            result = str(eval(expression))
+            result = str(ast.literal_eval(expression))
             return (result, False)
         except:
             return ("Sorry, I couldn't understand your question.", False)
@@ -34,12 +34,28 @@ def answer_question(query, data_set):
     # Extract the answer from the API response
     answer = data.get("AbstractText")
 
-    # If there is no answer, return a default message
+    # If there is no answer, try searching on Wikipedia
+    if not answer:
+        try:
+            answer = wikipedia.summary(query)
+        except:
+            pass
+
+    # If there is still no answer, return a default message
     if not answer:
         # Suggest a query based on the closest match in the data set
-        closest_match = process.extractOne(query, data_set.keys(), scorer=fuzzywuzzy.fuzz.token_sort_ratio)
+        closest_match = process.extractOne(query, data_set.keys(), scorer=fuzz.token_sort_ratio)
         if closest_match[1] >= 60:
             return (f"Did you mean '{closest_match[0]}'?\n{data_set[closest_match[0]]}", False)
+        
+        print("Sorry, I couldn't find an answer to your question. Would you like to add the answer manually? (yes/no):")
+        manual_input = input()
+        if manual_input.lower() == "yes":
+            manual_answer = input("What is the answer to the question: ")
+            data_set[query] = manual_answer
+            with open("data_set.txt", "a") as f:
+                f.write(f"{query}:{manual_answer}\n")
+            return (manual_answer, False)
         return ("Sorry, I couldn't find an answer to your question.", False)
 
     # Add the answer to the data set for future use
@@ -66,22 +82,20 @@ def print_with_typing_animation(summary):
         print(char, end="", flush=True)
         time.sleep(delay)
 
-
 print_with_typing_animation(colored(f"Welcome to the Question and Answer Program! ({datetime.now().strftime('%Y-%m-%d %H:%M:%S')})", "magenta"))
 print_with_typing_animation(Fact.generate_fun_fact())
 
 # Example usage
 while True:
-    question = input("\nWhat's your question? (type 'exit' to quit) ")
+    question = input("\nWhat's your question? (type 'exit' to quit): ")
 
-    # Allow the user to exit the program
     if question.lower() == "exit":
         break
 
     answer, is_data_set = answer_question(question, data_set)
 
-    # Display the answer with a typing animation
     for char in answer:
         print(colored(char, "yellow") if not is_data_set else colored(char, "green"), end="", flush=True)
-        time.sleep(random.uniform(0.001, 0.01))
+        time.sleep(random.uniform(0.01, 0.03))
     print("\n")
+
